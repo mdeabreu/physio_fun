bee01af0523c0e6be78bb911a5f66e0020cbdec3
Matthew DeAbreu <Matthew.DeAbreu@gmail.com>
2025-08-12 09:51:26 -0700

    restart container

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker build -t physio_fun .
 docker stop physio_fun
 docker rm physio_fun
-docker run -d --name physio_fun -p 5173:80 physio_fun
+docker run -d --name physio_fun --restart always -p 5173:80 physio_fun
